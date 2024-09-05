38ac1e0c2cc65b084182142a99df7738a82d9b7f
tiantian04 <1057164772@qq.com>
2024-09-05 15:58:42 +0800

    Resolve merge conflicts

---
+++ src/main/java/com/tiantian/tianAIda/constant/FileConstant.java
@@ -0,0 +1,16 @@
+package com.tiantian.tianAIda.constant;
+
+/**
+ * 文件常量
+ *
+ * @author <a href="https://github.com/litiantian">tiantian</a>
+ * @from <a href="https://tiantian.icu">编程导航知识星球</a>
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://tiantian.icu";
+}
---
+++ src/main/java/com/tiantian/tianAIda/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.tiantian.tianAIda.mapper;
+
+import com.tiantian.tianAIda.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author <a href="https://github.com/litiantian">tiantian</a>
+ * @from <a href="https://tiantian.icu">编程导航知识星球</a>
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
